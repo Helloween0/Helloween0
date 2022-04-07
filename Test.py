5b80067c6aa6eff911a7ba0333cf42029c61737e
Helloween0 <93432323+Helloween0@users.noreply.github.com>
2022-04-07 13:26:33 +0530

    Update and rename Test to Test.py

---
+++ Test.py
@@ -0,0 +1,51 @@
+GNU nano 4.6              pyphisher.py
+# -*- coding: UTF-8 -*-
+# Tool    : PyPhisher
+# Version : 1.6
+# Author  : KasRoudra
+# Github  : https://github.com/KasRoudra
+# Contact : https://m.me/KasRoudra
+# PyPhisher is a phishing tool in python
+# Facebook Phishing, Github Phishing, Instagram Phishing and 40>
+# Portable file/script
+# If you copy open source code, consider giving credit
+
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+                            Preamble
+
+  The GNU General Public License is a free, copyleft license for
+software and other kinds of works.
+
+  The licenses for most software and other practical works are >
+to take away your freedom to share and change the works.  By co>
+the GNU General Public License is intended to guarantee your fr>
+share and change all versions of a program--to make sure it rem>
+software for all its users.  We, the Free Software Foundation, >
+GNU General Public License for most of our software; it applies>
+any other work released this way by its authors.  You can apply>
+your programs, too.
+
+  When we speak of free software, we are referring to freedom, >
+price.  Our General Public Licenses are designed to make sure t>
+have the freedom to distribute copies of free software (and cha>
+them if you wish), that you receive source code or can get it i>
+want it, that you can change the software or use pieces of it i>
+free programs, and that you know you can do these things.
+
+  To protect your rights, we need to prevent others from denyin>
+these rights or asking you to surrender the rights.  Therefore,>
+certain responsibilities if you distribute copies of the softwa>
+you modify it: responsibilities to respect the freedom of other>
+
+  For example, if you distribute copies of such a program, whet>
+gratis or for a fee, you must pass on to the recipients the same
+
+^G Get Help ^O Write Out^W Where Is ^K Cut Text ^J Justify
+^X Exit     ^R Read File^\ Replace  ^U Paste Tex^T To Spell
